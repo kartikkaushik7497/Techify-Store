87be535a58b41582515b199fd3bb26952f0cfc07
Kartik Kaushik <116940711+kartikkaushik7497@users.noreply.github.com>
2023-05-05 00:05:41 +0530

    Revert "Dev"

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,8 @@ app.use(express.json());
 
 //Route Imports 
 const product = require("./routes/productRoute");
-const user = require("./routes/userRoute");
 
 app.use("/api/v1",product);
-app.use("/api/v1",user);
 
 //Middleware for Errors
 app.use(errorMiddleware);
