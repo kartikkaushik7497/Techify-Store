427c8b7b6f96879cbd84980b713b50ff91fa587a
kartikkaushik7497 <kartikkaushik7497@gmail.com>
2024-02-16 20:05:33 +0530

    Added Cloudinary image support to update User image

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,15 +8,11 @@ const cloudinary = require("cloudinary");
 
 //Register User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-
-  const myCloud = await cloudinary.v2.uploader.upload(
-    req.body.avatar,
-    {
-      folder: "avatars",
-      width: 150,
-      crop: "scale",
-    }
-  );
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "avatars",
+    width: 150,
+    crop: "scale",
+  });
   const { name, email, password } = req.body;
 
   const user = await User.create({
@@ -188,7 +184,24 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     email: req.body.email,
   };
 
-  //We will add cloudinary later
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+
+    const imageId = user.avatar.public_id;
+
+    await cloudinary.v2.uploader.destroy(imageId);
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
 
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
